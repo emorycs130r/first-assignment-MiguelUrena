3ce7b5b557de7c7ac07611fd81a19d16bc3538c7
MiguelUrena <78118827+MiguelUrena@users.noreply.github.com>
2021-02-06 00:24:03 -0500

    Add files via upload
    Both Assignments Done

--- easy_assign.py
+++ easy_assign.py
@@ -14,21 +14,32 @@ DO NOT EDIT THE FUNCTION NAMES.
 
 '''
 
-def append_two_strings(string_1, string_2):
+def append_two_strings(string_1,string_2):
+    string_3= string_1 + string_2
+    print("String 1 is: ",string_1)
+    print("String 2 is: ",string_2)
+    print("When appended the result is: ",string_3)
+    return
+    
 
-    pass
+
+def append_character(string_1,char_1):
+    string_4= string_1 + char_1
+    print("String 1 is: ",string_1)
+    print("Character 1 is: ",char_1)
+    print("When appended the result is: ",string_4)
+    return 
 
 
-def append_character(string_1, char_1):
-
-    pass
-
-
-def append_num_to_string(string_1, num_1):
-
-    pass
-
+def append_num_to_string(string_1,num_1):
+    string_5= string_1 + num_1
+    print("String 1 is: ",string_1)
+    print("Number 1 is: ",num_1)
+    print("When appended the result is: ",string_5)    
+    return
 
 if __name__ == "__main__":
+    append_two_strings("Hello"," World")
+    append_character("Hello","!")
+    append_num_to_string("Hello"," 15")
     
-    pass
--- medium_assign.py
+++ medium_assign.py
@@ -12,31 +12,41 @@ DO NOT EDIT THE FUNCTION NAMES.
 
 
 '''
-
+import math
 def expression_1(x):
-
+    print("X is equal to: ",x)
+    var_1=math.pow(x,3) - (x*2 + math.pow(x,2)) - 100
+    print("The Answer is: ",var_1)
     '''
     
         Write a code that returns value for the following expression: x^3 - (2x + x^2) - 100 
     
     '''
-
-    pass
+    return
 
 def expression_2(x, y):
-
+    print("X is equal to: ",x)
+    print("Y is equal to: ",y)
+    var_2=(math.pow(x,4)/(2*y)) - 3*x*y + ((6*y)/(7*x))
+    print("The Answer is: ", var_2)
     '''
         Write code that returns only the integer value of the following expression: (x^4 / 2y) - (3xy) + (6y / 7x)
     '''
-
-    pass
+    return
 
 
 def expression_3(x, y):
+    print("X is equal to: ",x)
+    print("Y is equal to: ",y)
+    var_3=(math.pow(x,3)+math.pow(y,2))/(math.pow(x,2)-math.pow(y,3))
+    print("The Answer is: ", var_3)
     '''
         Write code that returns the value of the following expression: (x^3 + y^2) / (x^2 - y^3)
     '''
+    return
+
 
 if __name__ == "__main__":
-    
-    pass
+    expression_1(3)
+    expression_2(2,3)
+    expression_3(1,2)
